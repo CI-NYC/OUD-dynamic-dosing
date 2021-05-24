909d67ae292d8f634e883c5a87a81ce87ae424a2
Alicia T. Singham Goodwin <asinghamgoodwin@gmail.com>
2021-05-23 22:21:15 -0400

    add in imputation code, update 05 file

--- 03-multiple-imputation.R
+++ 03-multiple-imputation.R
@@ -1,19 +1,26 @@
 #multiple imputation
 
 library(mice)
-library(mitools)
-
-#Note: `patients` is a data frame including all individual-level data, with one entry per patient
+#library(mitools)
 
 #set random seed for reproduceability
 set.seed(123)
 
+#Note: `patients_with_outcomes_02` is a data frame including all individual-level data, with one entry per patient
+#We will be imputing values for all of the variables in `comorbidities`.
+#  There is no missing data among the `demog` (demographic) variables
+#  We will not be imputing outcomes, but using the relapse_date to help our imputations (the other outcomes are redundant)
+
 #how many missing values are there? --> no columns are missing for more than 8% of patients
-# map(select(patients, all_of(demog), all_of(comorbidities)), ~mean(is.na(.)))
+# map(select(patients_with_outcomes_02, all_of(comorbidities)), ~mean(is.na(.)))
+
+patients_to_impute = patients_with_outcomes_02 %>%
+  select(all_of(demog), all_of(treatment_info), all_of(comorbidities), relapse_date)
+
 
 #---------------- Get set up for imputation -----------------#
 
-setup = mice(patients, maxit = 0) 
+setup = mice(patients_to_impute, maxit = 0) 
 my_methods = setup$method
 my_predMatrix = setup$predictorMatrix
 
@@ -23,18 +30,16 @@ my_predMatrix[, c("project")] = 0 #exclude b/c completely determined by site
 my_predMatrix[, c("end_of_detox")] = 0 #exclude b/c completely determined by rand_dt
 
 #alter the methods matrix, to exclude any variables we don't want to impute
-my_methods[all_of(outcomes)] = ""
+#NOTE: this is just to be safe -- all of the following should have complete information  (no missing)
+my_methods[all_of(demog)] = ""
 my_methods[all_of(treatment_info)] = ""
+my_methods[relapse_date] = ""
+
 
 #---------------- Impute missing data -----------------#
 
-#imputed = mice(select(patients, all_of(demog), ivdrug), method = my_methods, predictorMatrix = my_predMatrix, m = 5)
-imputed = mice(patients, method = my_methods, predictorMatrix = my_predMatrix, m = 10)
+patients_imputed_03 = mice(patients_to_impute, method = my_methods, predictorMatrix = my_predMatrix, m = 5)
 
-# LOOK UP:
-#mi tools: MI combine function
-
-patient_data_after_imputation_03 = complete(imputed)
-
-#---------------- X -----------------#
-#---------------- X -----------------#
+# #NOTE: this is how to access the different versions of the imputed data later
+# p1 = complete(patients_imputed_03, action = 1)
+# p2 = complete(patients_imputed_03, action = 2)
--- 05-one-step-data-prep.R
+++ 05-one-step-data-prep.R
@@ -1,4 +1,4 @@
-#one step data prep: only need to run this one R script to do all data prep and create datasets
+#one step data prep: you only need to run this one R script to do all data prep and create datasets before analysis
 
 #---------------- X -----------------#
 #---------------- X -----------------#
@@ -6,17 +6,18 @@
 
 source("00-codebook.R")
 source("01-initial-data-cleaning.R")
-#source("02-defining-relapse-outcomes.R")
-#source("03-multiple-imputation.R")
+source("02-defining-relapse-outcomes.R")
+source("03-multiple-imputation.R")
 #source("04-ltmle-prep.R")
 
-# 
-# 
-# save(codebook,
-#      initial_data_cleaning_no_outcomes_01, patient_data_after_imputation_03
-#      
-#      clean_combined_data,
-#      patients,
-#      weeks,
-#      demog, comorbidities, treatment_info, outcomes, visit_data, weekly_indicators,
-#      file = "clean_combined_data.RData")
+
+#MAKE 2 COPIES, one imputed and one not. Maybe leave the un-imputed one commented out
+save(
+  codebook,
+  initial_data_cleaning_no_outcomes_01,
+  patients_with_outcomes_02, visits_with_outcomes_02, weeks_with_outcomes_02,
+  patients_imputed_03,
+  patients, visits, weeks,
+  demog, comorbidities, treatment_info, outcomes, visit_data, weekly_indicators,
+  file = "clean_combined_data.RData"
+)
